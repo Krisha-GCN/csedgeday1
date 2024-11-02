ed33f91761968c83cd3c3c4a734f8fc163eb4e99
student <student@provisions-MacBook-Air.local>
2024-11-02 14:05:31 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, json
+import requests
+
+app = Flask(__name__)
+
+# apiData = [
+#     {
+
+#     }
+# ]
+
+@app.route("/<name>/<day>")
+def hello_world(name, day):
+    return "Hello" + name + "today is" + day
+
+@app.route("/<num>/")
+def number(num):
+    if int(num) > 10: 
+        return num + " is greater than 10"
+    return num + " is less than 10"
+
+@app.route("/penguins")
+def penguin():
+    return 
+    # penguin = requests.get("http://100.114.3.84:5000/11")
+    # return "olivias api said: " + penguin.text
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001, host = '0.0.0.0')
